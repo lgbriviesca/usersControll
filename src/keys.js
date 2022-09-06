f84e8105ed7224680870f029dcf561186f5a5dfc
lgbriviesca <98791864+lgbriviesca@users.noreply.github.com>
2022-09-05 19:01:12 -0500

    3r

--- src/keys.js
+++ src/keys.js
@@ -1,9 +1,19 @@
 module.exports = {
   database: {
     connectionLimit: 10,
+    host: 'localhost',
+    user: 'root',
+    password: "password",
+    database: 'database_links',
+  },
+};
+
+/* module.exports = {
+  database: {
+    connectionLimit: 10,
     host: 'us-cdbr-east-06.cleardb.net',
     user: 'ba3c3899876682',
     password: "49d65eee",
     database: 'heroku_592fd0a9854f37d',
   },
-};
+}; */
